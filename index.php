c040446bef786b473626d5aa6b8a7db713f80d7d
rizredvlo7zer0 <50101349+rizredvlo7zer0@users.noreply.github.com>
2019-12-12 00:09:09 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head><title></title></head>
+<body>
+
+<style>
+body {
+  background: url(../../eskipaper.com/images/dark-anime-backgrounds-2.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color: white;
+}
+.twitter a{
+  text-decoration: none;
+  font-family: Arial, sans-serif  ;
+  font-size: 50px;
+  text-shadow: grey 0px 0px 10px;
+}
+ 
+ 
+}</style>
+
+<center>
+<table width="100%" height="100%"><tbody><tr><td align="center"><br>
+<div class="twitter"><a href="https://github.com/rizredvlo7zer0/Rizredvlo_apk/blob/master/Rizredvlo_9.2.apk"><font color="red"><b>Kalo Mo DOR Buka Di Apk Anjing .W Gk Dapet Duit klo Lu Buka Pke Link Tolol</b></font></a></div><font color="red"><b>
+<div id="bar" style="position: fixed; width: 100%; bottom: 0px; font-family: Tahoma; height: 20px; color: white; font-size: 13px; left: 0px; border-top: 2px solid darkred; padding: 30px; background-color: #000"><b>By Admin Rizred Vlo 7zer0✬ </b></font></b></div><b><font color="red"><b><br></b>
+<font color="white"><b></b></font></font><b><br></b>
+ 
+
+
+
+</b></b></font></td></tr></tbody></table></center></body></html>
+
+</body>
+</html>
